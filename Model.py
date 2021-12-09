2a0421613e5f9a24feaa54e302100f4799986d49
tejukandula9 <46721607+tejukandula9@users.noreply.github.com>
2021-12-08 20:47:34 -0800

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor, plot_tree
+from sklearn import tree
+from matplotlib import pyplot as plt
+import plotly.express as px
+
+# Creating breeds Dict
+def create_breeds_dict(breeds):
+    breeds_dict = {}
+    for col in breeds:
+        curr_dict = dict.fromkeys(breeds[col].dropna(), col)
+        breeds_dict.update(curr_dict)
+    breeds_dict['Chihuahua Shorthair']='Toy'
+    breeds_dict['Chihuahua Longhair']='Toy'
+    breeds_dict['Pit Bull']='Pit Bull'
+    breeds_dict['German Shepherd']='Herding'
+    breeds_dict['Australian Kelpie']='Herding'
+    breeds_dict['Doberman Pinsch'] = 'Working'
+    breeds_dict['Alaskan Husky'] = 'Working'
+    breeds_dict['Staffordshire'] = 'Terrier'
+    breeds_dict['Collie Smooth'] = 'Herding'
+    breeds_dict['Catahoula'] = 'Working'
+    breeds_dict['Presa Canario'] = 'Herding'
+    breeds_dict['Black Mouth Cur'] = 'Herding'
+    breeds_dict['Anatol Shepherd'] = 'Working'
+    return breeds_dict
+
+breeds = pd.read_csv('dog_breeds.csv')
+breeds = breeds.loc[:, 'Herding':'Working']
+breeds_dict = create_breeds_dict(breeds)
+
+def find_breed_type(breed):
+    if 'Mix' in breed:
+        breed = breed.replace(' Mix', '')
+    if '/' in breed:
+        breed_list = breed.split('/', 1)
+        for breed in breed_list:
+            if breed in breeds_dict:
+                return breeds_dict[breed]
+            if 'Hound' in breed:
+                return 'Hound'
+            if 'Poodle' in breed:
+                return 'Non-Sporting'
+        return 'Not Classified'
+    if 'Bulldog' in breed:
+        breed = 'Bulldog'
+    if 'Dachshund' in breed:
+        breed = 'Dachshund'
+    if 'Pit Bull' in breed:
+        breed = 'Pit Bull'
+    if 'Chesa Bay' in breed:
+        breed = 'Chesapeake Bay Retriever'
+    if 'Queensland Heeler' in breed:
+        breed = 'Australian Cattle Dog'
+    if breed in breeds_dict:
+        return breeds_dict[breed]
+    if 'Terrier' in breed:
+        return 'Terrier'
+    if 'Toy' in breed or 'Miniature' in breed:
+        return 'Toy'
+    if 'Poodle' in breed:
+        return 'Non-Sporting'
+    if 'Hound' in breed:
+        return 'Hound'
+    return 'Not Classified'
+
+def create_dogs_df():
+    dogs = pd.read_csv('dog_data.csv')
+    dogs['Breed Type'] = dogs['Breed'].map(find_breed_type)
+    dogs = dogs[dogs['Outcome Type'].isin(['Adoption', 'Euthanasia'])]
+
+    # Clean Variables to reduce categories
+    dogs = dogs[dogs['Sex upon Outcome'] != 'Unknown']
+    dogs['Adoption Status'] = np.where(dogs['Outcome Type'] == 'Adoption', 'Adopted', 'Euthanised')
+    dogs['Sex'] = np.where(dogs['Sex upon Outcome'].str.contains('Female'), 'Female', 'Male')
+    dogs['Fixed_Status'] = np.where(dogs['Sex upon Outcome'].str.contains('Intact'), 'Intact', 'Fixed')
+    dogs['Pitbull_Status'] = np.where(dogs['Breed Type'].str.contains('Pit Bull'), 'Pit Bull', 'Not Pit Bull')
+    dogs['Condition_Status'] = np.where(dogs['Intake Condition'].str.contains('Normal'), 'Normal', 'Not Normal')
+    dogs['Senior_Status'] = np.where(dogs['Age upon Outcome (months)']<132, 'Not Senior', 'Senior')
+    dogs.reset_index(drop=True, inplace = True)
+
+    # Create Dummy Variables
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Sex'], prefix='Sex')], axis=1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Fixed_Status'], prefix='Fixed_Status')], axis = 1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Pitbull_Status'], prefix='Pitbull_Status')], axis=1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Condition_Status'], prefix='Condition_Status')], axis=1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Senior_Status'], prefix='Senior_Status')], axis=1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Breed Type'], prefix='Breed_Type')], axis=1)
+    dogs = pd.concat([dogs, pd.get_dummies(dogs['Color'], prefix='Color')], axis=1)
+    return dogs
+
+def create_model(x_vals):
+    dogs = create_dogs_df()
+    x = dogs[x_vals]
+    y = dogs['Adoption Status']
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.30)
+
+    dtree =  DecisionTreeClassifier(max_depth=2)
+    dtree.fit(x_train,y_train)
+    return (dtree, x.columns)
+
+def visualize_tree(x_vals):
+    mdl_vals = create_model(x_vals)
+    model = mdl_vals[0]
+    cols = mdl_vals[1]
+    fig = plt.figure(figsize=(3,1))
+    tree.plot_tree(model, feature_names = cols, class_names = ['Adopted', 'Euthanised'], filled = True, proportion = True, rounded = True)
+
+def find_importance(x_vals):
+    model_info = create_model(x_vals)
+    model = model_info[0]
+    col_names = model_info[1]
+    importance = pd.Series(model.feature_importances_, index = col_names).sort_values(ascending=False).to_frame()
+    importance.reset_index(inplace=True)
+    importance.columns = ['Factor', 'Importance Level']
+    return importance
+    
+def get_col_names(substr):
+    cols = list(create_dogs_df().columns)
+    cols_subset = [col for col in cols if substr in col]
+    return cols_subset
---
+++ app.py
@@ -0,0 +1,101 @@
+from Model import *
+from plotly.tools import mpl_to_plotly
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+import io
+import base64
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+fig = visualize_tree(['Sex_Male', 'Fixed_Status_Fixed', 'Pitbull_Status_Pit Bull', 'Condition_Status_Normal', 'Senior_Status_Senior'])
+plt.savefig('decision_tree.png',dpi=300, bbox_inches = "tight")
+
+def get_factors_list(factors):
+    if 'Breed_Type_' in factors:
+        new_cols = get_col_names('Breed_Type_')
+        factors.remove('Breed_Type_')
+        factors += new_cols
+    if 'Color_' in factors:
+        new_cols = get_col_names('Color_')
+        factors.remove('Color_')
+        factors += new_cols
+    return factors
+
+app.layout = html.Div(children=[
+    dbc.Alert(
+            "Error: Must Choose Atleast One Input for Decision Tree",
+            id="alert-auto",
+            is_open=False,
+            duration=4000,
+            color="danger"
+        ),
+    html.H1(children='Adoption/Euthanization Model'),
+    html.Div(children=[
+        html.Img(id='example'),
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        dcc.Checklist(
+            id = 'factors',
+            labelStyle={'display': 'block'},
+            options=[
+                {'label': 'Sex', 'value': 'Sex_Male'},
+                {'label': u'Fixed Status', 'value': 'Fixed_Status_Fixed'},
+                {'label': 'Intake Condition', 'value': 'Condition_Status_Normal'},
+                {'label': 'Senior Status', 'value': 'Senior_Status_Senior'},
+                {'label': 'Age', 'value': 'Age upon Outcome (months)'},
+                {'label': 'Breed Type', 'value': 'Breed_Type_'},
+                {'label': 'Color', 'value': 'Color_'}
+            ],
+            value=['Sex_Male', 'Fixed_Status_Fixed', 'Condition_Status_Normal', 'Senior_Status_Senior', 
+            'Age upon Outcome (months)', 'Breed_Type_', 'Color_']
+        )
+    ], style={'display': 'flex', 'flex-direction': 'row'}),
+    dcc.Graph(id='importance')
+])
+
+# Output Tree Diagram
+@app.callback(
+    dash.dependencies.Output('example', 'src'),
+    dash.dependencies.Input('factors', 'value')
+)
+def update_figure(factors):
+    buf = io.BytesIO() # in-memory files
+    if len(factors) <= 0:
+        fig = plt.figure(figsize=(3,2))
+    else:
+        factors = get_factors_list(factors)
+        visualize_tree(factors)
+    plt.savefig(buf, format = "png",dpi=300, bbox_inches = "tight") # save to the above file object
+    plt.close()
+    data = base64.b64encode(buf.getbuffer()).decode("utf8") # encode to html elements
+    return "data:image/png;base64,{}".format(data)
+
+@app.callback(
+    dash.dependencies.Output("alert-auto", "is_open"),
+    dash.dependencies.Input('factors', 'value'),
+    [dash.dependencies.State("alert-auto", "is_open")]
+)
+# Toggle alert if no factors are chosen
+def toggle_alert(factors, is_open):
+    if len(factors) <= 0:
+        return not is_open
+    return is_open
+
+#Output Importance Barplot
+@app.callback(
+    dash.dependencies.Output('importance', 'figure'),
+    dash.dependencies.Input('factors', 'value')
+)
+def update_figure(factors):
+    factors = get_factors_list(factors)
+    importance = find_importance(factors)
+    return px.bar(importance, x = 'Factor', y = 'Importance Level')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
